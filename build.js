9bc43481bac8b717952d4bd08bb22d944fb28af5
Dylan Blokhuis <dylanblokhuis3@hotmail.com>
2020-01-25 21:57:06 +0100

    added size prop

--- build.js
+++ build.js
@@ -5,6 +5,20 @@ const fs = require('fs-extra')
 
 const handleComponentName = name => name.replace(/\-(\d+)/, '$1')
 
+const component = (icon) =>
+`<script>
+  export let size = "100%";
+
+  if (size !== "100%") {
+    size = size.slice(-1) === 'x' 
+          ? size.slice(0, size.length -1) + 'em'
+          : parseInt(size) + 'px';
+  }
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="none" viewBox="0 0 24 24"  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-${icon.name}">${feather[icon.name]}</svg>
+`
+
 const icons = Object.keys(feather).map(name => ({
   name,
   pascalCasedComponentName: pascalCase(`${handleComponentName(name)}-icon`),
@@ -12,11 +26,9 @@ const icons = Object.keys(feather).map(name => ({
 }))
 
 Promise.all(icons.map(icon => {
-  const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24"  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-${icon.name}">${feather[icon.name]}</svg>`
-  const component = `${svg}`
   const filepath = `./src/icons/${icon.pascalCasedComponentName}.svelte`
   return fs.ensureDir(path.dirname(filepath))
-    .then(() => fs.writeFile(filepath, component, 'utf8'))
+    .then(() => fs.writeFile(filepath, component(icon), 'utf8'))
 })).then(() => {
   const main = icons
     .map(icon => `export { default as ${icon.pascalCasedComponentName} } from './icons/${icon.pascalCasedComponentName}.svelte'`)
